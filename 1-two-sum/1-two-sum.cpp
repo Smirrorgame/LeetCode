bacc3db0bb92013ae53bb043d582b1ebedf48a4a
Micha Halla <48619930+Smirrorgame@users.noreply.github.com>
2022-06-29 17:39:18 +0200

    Time: 755 ms (12.46%), Space: 10.3 MB (69.94%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> null_arr;
-        for (long unsigned int i = 0; i < nums.size(); i++)
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
         {
-            for (long unsigned int j = i + 1; j < nums.size(); j++)
+            for (int j = i + 1; j < nums.size(); j++)
             {
                 // cout << "i:" << i << " j:" << j << endl;
                 if (nums[i] + nums[j] == target)
                 {
-                    vector<int> ret = {(int)i, (int)j};
+                    vector<int> ret = {i, j};
                     return ret;
                 }
             }
-        }
-        return null_arr;
+        }return {};
     }
 };
