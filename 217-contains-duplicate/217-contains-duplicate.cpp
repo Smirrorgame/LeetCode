9b7c09bbf960ec78812be311cf029673b0fdd696
Micha Halla <48619930+Smirrorgame@users.noreply.github.com>
2022-07-01 12:25:47 +0200

    Time: 133 ms (56.14%), Space: 51.5 MB (50.96%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> entryCount;
+        unordered_map<int, int> entryCount;
         for (int i = 0; i < (int)nums.size(); i++)
         {
             if (entryCount.count(nums[i]) == 0)
